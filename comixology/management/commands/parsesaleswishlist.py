28df8109478fd1440490b2b3e6f0c3859ef0cf4d
Javi Manzano <javi.manzano.oller@gmail.com>
2018-09-01 20:41:08 +0100

    New management command for looking for wishlist comics on sale

--- comixology/management/commands/parsesaleswishlist.py
+++ comixology/management/commands/parsesaleswishlist.py
@@ -4,7 +4,7 @@ from comixology import tasks
 
 
 class Command(BaseCommand):
-    help = 'Parse the current comixology sales'
+    help = 'Parse the current comixology sales and look for comics in the wishlist'
 
     def handle(self, *args, **options):
         tasks.parse_sales_wishlist()
