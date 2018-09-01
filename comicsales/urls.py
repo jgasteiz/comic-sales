8369d7d7f5286c34b8308545a2c5f6107d098102
Javi Manzano <javi.manzano.oller@gmail.com>
2018-09-01 17:28:43 +0100

    Add a new api endpoint for getting all current comic sales

--- comicsales/urls.py
+++ comicsales/urls.py
@@ -23,6 +23,7 @@ from public import views as public_views
 urlpatterns = [
     path('admin/', admin.site.urls),
 
+    url(r'^api/all-sales$', public_views.api_all_sales, name='api_all_sales'),
     url(r'^api/last-sales$', public_views.api_last_sales, name='api_last_sales'),
 
     url(r'^$', public_views.sale_list, name='sale_list'),
--- public/views.py
+++ public/views.py
@@ -16,6 +16,13 @@ def sale_list(request):
     return render(request, 'public/sale_list.html', context=context)
 
 
+def api_all_sales(request):
+    today = timezone.datetime.today().date()
+    comic_sales = Sale.objects.filter(date_end__gte=today)
+    last_sales = [f'{s.title}, ends on {s.date_end}' for s in comic_sales]
+    return HttpResponse(json.dumps(last_sales), content_type='application/json')
+
+
 def api_last_sales(request):
     today = timezone.datetime.today().date()
     comic_sales = Sale.objects.filter(date_end__gte=today, created_at__gte=today - datetime.timedelta(days=7))
