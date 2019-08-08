d5b00cc5aa47910c76e3d680cc6780965edf807d
Javi Manzano <javi.manzano.oller@gmail.com>
2019-08-08 17:32:15 +0100

    Remove pipenv, use sqlite

---
+++ README.md
@@ -0,0 +1,9 @@
+# Comic sales
+
+## Installation
+
+- `mkvirtualenv comic-sales`
+- `pip install -r requirements.txt`
+- `./manage.py migrate`
+- `./manage.py parsesales`
+- `./manage.py runserver`
--- comicsales/settings.py
+++ comicsales/settings.py
@@ -12,14 +12,9 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
-import dotenv
-from dotenv import load_dotenv
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -81,7 +76,7 @@ WSGI_APPLICATION = "comicsales.wsgi.application"
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {"ENGINE": "django.db.backends.postgresql", "NAME": "comic-sales"}
+    "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": "comic-sales.db"}
 }
 
 # Password validation
